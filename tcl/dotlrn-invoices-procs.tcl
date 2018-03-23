93ae83c2738d05f5cb5eabadf9cb0a02f1ab5ce0
Hector Romojaro <hector.romojaro@gmail.com>
2018-03-23 23:53:01 +0000

    Fix more typos

--- tcl/dotlrn-invoices-procs.tcl
+++ tcl/dotlrn-invoices-procs.tcl
@@ -235,7 +235,7 @@ ad_proc -public dotlrn_invoices::remove_portlet {
     args
 } {
     A helper proc to remove the underlying portlet from the given portal.
-    This is alot simpler than add_portlet.
+    This is a lot simpler than add_portlet.
 
     @param portal_id
     @param args An ns_set.
